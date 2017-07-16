22d0d29c4dd60eeb611bf206d65d257dd5b75163
anatol-blackforest <anatolblackforest@gmail.com>
2017-07-16 16:03:40 +0300

    comments

--- app.js
+++ app.js
@@ -72,15 +72,18 @@ let editor = function(err, req, res, typeOfEdit) {
 		});
 	} else {
         if (req.file && req.file.mimetype && req.file.mimetype.indexOf('image') == -1){
+			//если загруженный файл - НЕ рисунок
 			list((err, films) => {
 				render(isAdmin, res, films, messages[1]);
 			});
 		} else {
 			if(typeOfEdit == "add"){
+				//при добавлении
                 add(req, res, () => {
 					res.redirect('/');
 				});
 			} else if (typeOfEdit == "change"){
+				//при редактировании
 				change(req, res,() => {
 					res.redirect('/');
 				});
